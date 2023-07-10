1e62902c92c1f96316d97e6e0136c372b521e6a3
nuria-marmol <numarbru@gmail.com>
2023-07-10 15:44:40 +0200

    fixed checkbox bug

--- src/App.vue
+++ src/App.vue
@@ -82,11 +82,6 @@
     }
   }
 
-  function toggleCheckbox(task) {
-      task.checked = checkbox.value
-      updateStoragedTasks()
-  }
-
   /**
   * Enabling filtering by checked tasks
   */
@@ -258,34 +253,34 @@
           class="tasks__item"                    
         >
           <div>
-              <input
-                  type="checkbox"
-                  :id="task.id"
-                  class="tasks-item__checkbox"                 
-                  v-model="checkbox"
-                  @change="toggleCheckbox(task)"
-              />
-              <!-- Changing background colour when checked -->
-              <div 
-                  class="tasks-item__custom-checkbox"
-                  :class="{'tasks-item__custom-checkbox--checked': task.checked }"
-              />
-              <!-- Showing when checked -->
-              <img           
-                  class="tasks-item__checkmark"
-                  :class="{ 'tasks-item__checkmark--show': task.checked }"       
-                  src="./assets/icons/checkmarks.png"
-                  alt="checkmark"
-              />
+            <label :for="task.id"></label>
+            <input
+              type="checkbox"
+              :id="task.id"
+              class="tasks-item__checkbox"                
+              v-model="task.checked"              
+            />            
+            <!-- Changing background colour when checked -->
+            <div 
+              class="tasks-item__custom-checkbox"
+              :class="{'tasks-item__custom-checkbox--checked': task.checked }"
+            />
+            <!-- Showing when checked -->
+            <img           
+              class="tasks-item__checkmark"
+              :class="{ 'tasks-item__checkmark--show': task.checked }"       
+              src="./assets/icons/checkmarks.png"
+              alt="checkmark"
+            />
           </div>
           <div class="tasks-item__text-group">
-              <!-- Line-through when checked -->
-              <span :class="{ done: task.checked }">{{ task.title }}</span>
-              <img 
-                  src="./assets/icons/delete.png"
-                  alt="Eliminar tarea"
-                  @click="deleteTask(task)"
-              />
+            <!-- Line-through when checked -->
+            <span :class="{ done: task.checked }">{{ task.title }}</span>
+            <img 
+                src="./assets/icons/delete.png"
+                alt="Eliminar tarea"
+                @click="deleteTask(task)"
+            />
           </div>
         </li>  
       </draggable>     
